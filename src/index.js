4bc3413a9dd34aad1856e7d0758f01d0f9e1ddd1
Rupayan <rupayanc16@gmail.com>
2020-12-25 13:35:46 +0530

    ...

---
+++ src/index.js
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+const existingConfig = fs.existsSync('now.json');
+
+const questions = [];
+
+const buildConfig = () => {
+	inquirer
+		.prompt([
+			{
+				type: 'text',
+				name: 'name',
+				message: 'Name of the project',
+				default: path.basename(process.cwd())
+			},
+			{
+				type: 'list',
+				name: 'type',
+				message: 'Type of the project?',
+				choices: ['react', 'angular', 'svelte']
+			}
+		])
+		.then((ans) => {
+			console.log(ans);
+		});
+};
+
+if (existingConfig) {
+	inquirer
+		.prompt([
+			{
+				type: 'confirm',
+				name: 'overwrite',
+				message: 'now.json already exists. Overwrite?',
+				default: false
+			}
+		])
+		.then((ans) => {
+			console.log(ans);
+			if (ans.overwrite) {
+				buildConfig();
+			} else {
+				console.log('Exiting tool...');
+			}
+		});
+} else {
+	buildConfig();
+}
+
